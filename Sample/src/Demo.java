5ee9e2886f444b5de157abd351ad97e688ab5f19
tsuser <tsuser@sys>
2017-03-24 11:05:11 +0530

    Sample

---
+++ Sample/src/Demo.java
@@ -0,0 +1,20 @@
+
+public class Demo {
+	public static void printAll(String s){
+		 int i=0;
+		   if(s.length()<=0){
+		 
+		   return;
+		   
+		   }
+		   
+		   System.out.print(""+s.charAt(i++));
+		   
+		   printAll(s.substring(0,s.length()-1));
+		    
+		}
+	public static void main(String[] args) {
+	printAll("123");
+	}
+
+}
